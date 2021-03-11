e58f22c5282542607138591e1f0890c5060199bc
Carlota <carlotamorenogarcia@gmail.com>
2021-03-11 13:42:49 +0100

    Mas o menos pagina productos

--- main.js
+++ main.js
@@ -7,3 +7,22 @@ function enseñar(){/*Quitar o poner menur*/
    menuopen.classList.toggle('cnopen');
 }
 /********************************************MENU***********************************************/
+/*******************************************PRODUCTOS***********************************************/
+var inicio = 0;
+carrusel();
+
+function carrusel(){
+    var i;
+    var x = document.getElementsByClassName("mis");
+    for (i=0; i<x.length; i++){
+        x[i].style.display = "none";
+    }
+    inicio++;
+
+    if(inicio >x.length){
+        inicio = 1;
+    }
+    x[inicio-1].style  .display= "block";
+    setTimeout(carrusel, 2000);
+}
+/*******************************************PRODUCTOS***********************************************/
