eb0c3e19ef9267ff2fa7996a794941902669c3b0
Carlota <carlotamorenogarcia@gmail.com>
2021-03-11 03:46:26 +0100

    Hecho el menu div fondo intento logo

---
+++ main.js
@@ -0,0 +1,9 @@
+/********************************************MENU***********************************************/
+function enseñar(){/*Quitar o poner menur*/ 
+   let cn = document.getElementById('menunav');
+   cn.classList.toggle('cnopen');
+
+   let menuopen = document.getElementById('menotoggle');
+   menuopen.classList.toggle('cnopen');
+}
+/********************************************MENU***********************************************/
